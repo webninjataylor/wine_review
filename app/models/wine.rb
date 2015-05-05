290e7fc7ea5c90d30b9866d11e530a8bbf09fc0c
webninjataylor <webninjataylor@gmail.com>
2015-05-04 22:37:50 -0400

    Added validations to wine model.

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,30 @@
 class Wine < ActiveRecord::Base
+  
+  VARIETALS = [
+    'Barbera',
+    'Cabernet Sauvignon',
+    'Chardonnay',
+    'Gewürztraminer',
+    'Grenache',
+    'Malbec',
+    'Merlot',
+    'Nebbiolo',
+    'Petite Sirah',
+    'Pinot Grigio / Pinot Gris',
+    'Pinot Noir',
+    'Port',
+    'Rhone Blends',
+    'Riesling',
+    'Sangiovese',
+    'Sauvignon Blanc',
+    'Syrah / Shiraz',
+    'Tempranillo',
+    'Verdejo',
+    'Viognier',
+    'Zinfandel'
+  ]
+
+  validates :name, :year, :country, presence: true
+  validates :varietal, inclusion: { in: VARIETALS, message: "%{value} is not a valid varietal" }
+
 end
